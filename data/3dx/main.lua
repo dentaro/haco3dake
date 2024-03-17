907f7b80a71c8cfc73174cafb1efba0e951638dd
dentaro <77978725+dentaro@users.noreply.github.com>
2024-03-17 12:50:59 +0900

    Add files via upload

--- data/3dx/main.lua
+++ data/3dx/main.lua
@@ -1,5 +1,17 @@
-initstars()
-creobj2()
+p = 0.5
+s = 0.2
+
+creobj2(1,   -p,-p*2,0.0,  90,127,192, s)--cmy
+creobj2(1,  0.0,-p*2,0.0, 210,127,128, s)
+creobj2(1,    p,-p*2,0.0, 330,127, 96, s)
+
+creobj2(1,   -p,-p,0.0,  60,127,128, s)--cmy
+creobj2(1,  0.0,-p,0.0, 180,127,128, s)
+creobj2(1,    p,-p,0.0, 300,127,128, s)
+
+creobj2(1,   -p,0.0,0.0,   0,127,128, s)--rgb
+creobj2(1,  0.0,0.0,0.0, 120,127,128, s)
+creobj2(1,    p,0.0,0.0, 240,127,128, s)
 
 colangle = 0
 sw = 1
@@ -24,39 +36,52 @@ light = {
 }
 
 function _init()
-
+  camera.zoom = 1.0
+  camera.z = 1.0;
 end
 
 function _update()
+  -- if btnp(3) then 
+  --   -- creobj2(0)
+  --   camera.zoom = camera.zoom - 0.05
+  -- end
+  -- if btnp(4) then 
+  --   -- creobj2(1)
+  --   camera.zoom = camera.zoom + 0.05
+  -- end
+
+  -- if btnp(3) then 
+  --   camera.z = camera.z - 0.01  
+  -- end
+  -- if btnp(4) then 
+  --   camera.z = camera.z + 0.01  
+  -- end
 
 
-  if btnp(1)then 
-    camera.zoom = camera.zoom - 0.05  
-  end
-  if btnp(2) then 
-    camera.zoom = camera.zoom + 0.05  
+  if camera.zoom<0 then
+    camera.zoom =0.01
   end
 
   
 
+  if btnp(1) then 
+    sw = 1
+    camera.x  = camera.x - 0.05
+  end
+
+  if btnp(2) then   
+    sw = 1
+    camera.x  = camera.x + 0.05
+  end
+
+
   if btnp(3) then 
-    sw = 1
-    camera.anglex  = camera.anglex - 2
+    sw = 2
+    camera.y  = camera.y - 0.05
   end
-
   if btnp(4) then   
-    sw = 1
-    camera.anglex  = camera.anglex + 2
-  end
-
-
-  if btnp(6) then 
     sw = 2
-    camera.angley  = camera.angley - 2
-  end
-  if btnp(5) then   
-    sw = 2
-    camera.angley  = camera.angley + 2
+    camera.y  = camera.y + 0.05
   end
   
 
@@ -64,6 +89,5 @@ end
 
 function _draw()
   cls()
-
-  rendr2( camera.anglex, camera.angley, sw, camera.zoom)
+  rendr2( camera.x, camera.y, camera.z, camera.anglex, camera.angley, sw, camera.zoom)
 end
